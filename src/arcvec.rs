34ffcfa93467c12f617da47ede2e5fd500c8a4a2
silverstillisntgold <nickdilla13@gmail.com>
2025-08-13 21:05:25 -0400

    plurality

--- src/arcvec.rs
+++ src/arcvec.rs
@@ -16,38 +16,38 @@ impl<T> Clone for ArcVec<T> {
 }
 
 impl<T> ArcVec<T> {
-    /// Create a new `ArcVec` with a small default allocation.
+    /// Creates a new `ArcVec` with a small default allocation.
     #[inline]
     pub fn new() -> Self {
         Self::with_capacity(4)
     }
 
-    /// Call [`Vec::with_capacity`] with `capacity`.
+    /// Calls [`Vec::with_capacity`] with `capacity`.
     #[inline]
     pub fn with_capacity(capacity: usize) -> Self {
         let inner = Arc::new(Mutex::new(Vec::with_capacity(capacity)));
         Self { inner }
     }
 
-    /// Call [`Vec::is_empty`] after locking `self`.
+    /// Calls [`Vec::is_empty`] after locking `self`.
     #[inline]
     pub fn is_empty(&self) -> bool {
         self.inner.lock().unwrap().is_empty()
     }
 
-    /// Call [`Vec::push`] with `value` after locking `self`.
+    /// Calls [`Vec::push`] with `value` after locking `self`.
     #[inline]
     pub fn push(&self, value: T) {
         self.inner.lock().unwrap().push(value);
     }
 
-    /// Call [`Vec::extend`] with `iter` after locking `self`.
+    /// Calls [`Vec::extend`] with `iter` after locking `self`.
     #[inline]
     pub fn extend(&self, iter: impl IntoIterator<Item = T>) {
         self.inner.lock().unwrap().extend(iter);
     }
 
-    /// Extract the internal `Vec` of this `ArcVec`.
+    /// Extracts the internal `Vec` of this `ArcVec`.
     #[inline]
     pub fn into_inner(self) -> Vec<T> {
         Arc::into_inner(self.inner).unwrap().into_inner().unwrap()
