ffe939f41e3012fdcf28787a516e3a8453252970
silverstillisntgold <nickdilla13@gmail.com>
2026-07-22 08:32:48 -0400

    improve verifier.rs documentation

--- src/verifier.rs
+++ src/verifier.rs
@@ -12,54 +12,64 @@ pub struct DiffResult {
 }
 
 impl DiffResult {
-    /// Returns entries which only existed in the old [`Manifest`].
-    ///
-    /// The entries in this slice are missing from the new `Manifest`.
+    /// Returns the entries which only existed in the old [`Manifest`].
     #[inline]
     pub fn old_only(&self) -> &[Entry] {
         &self.old_only
     }
 
-    /// Returns entries which only existed in the new [`Manifest`].
-    ///
-    /// The entries in this slice are missing from the old `Manifest`.
+    /// Returns the entries which only existed in the new [`Manifest`].
     #[inline]
     pub fn new_only(&self) -> &[Entry] {
         &self.new_only
     }
 
-    /// Returns entries which exist in both [`Manifest`]'s, but are different.
+    /// Returns the entries which existed in both [`Manifest`]'s, but were different.
     ///
-    /// The `path` field of each `Entry` pair in this slice will always be equal.
+    /// The `path` field of each [`Entry`] pair in this slice will always be equal.
     #[inline]
     pub fn changed(&self) -> &[(Entry, Entry)] {
         &self.changed
     }
 }
 
-/// Compares the contents of two [`Manifest`]'s, returning `None` if they are the same.
+/// Compares the contents of two [`Manifest`]'s, returning [`None`] if they are the same.
 ///
 /// If any difference is found, a [`DiffResult`] is returned which contains information about
 /// how the two directories differ.
 ///
-/// The only difference which is allowed is the name of the root directory, because the name of
-/// a directory does not impact it's internal structure or the information it contains.
+/// Different root directory names/paths are allowed, because the name/path of a directory
+/// does not impact it's internal structure or the information it contains.
 #[inline(never)]
 pub fn verify(old_manifest: Manifest, new_manifest: Manifest) -> Option<DiffResult> {
+    debug_assert!(
+        old_manifest.entries.is_sorted_by_key(|e| e.path.as_str()),
+        "all old manifest entries should have been sorted before hashing"
+    );
+    debug_assert!(
+        new_manifest.entries.is_sorted_by_key(|e| e.path.as_str()),
+        "all new manifest entries should have been sorted before hashing"
+    );
+
     // Fast path: If both the total size and the hash of two directories are identical,
     // the probability of there being a mismatch of internal data between the two is so
-    // astronomically tiny that so we assume they are the same.
+    // astronomically tiny that we can safely assume they are the same.
     //
     // Size is compared first because it's the cheaper comparison, and is almost certainly going to
     // be different in cases where the hash is also different. We compare the bytes of the hash
     // instead of using the compare of the hash to take advantage of SIMD register compares.
+    // The specialized hash compare is optimized for cryptographic security we don't have need for,
+    // so it may not generate the fastest assembly on all platforms.
     //
-    // We've documented that it's fine for the name of the root directories to be different.
+    // We've documented that it's fine for the name/path of the root directories to be different.
     if old_manifest.directory_size == new_manifest.directory_size
         && old_manifest.directory_hash.as_bytes() == new_manifest.directory_hash.as_bytes()
     {
         return None;
     }
+
+    // If the `directory_size` and `directory_hash` values are not the same, then we know for
+    // certain that there **must** be some kind of difference between the two manifests.
 
     let mut result = DiffResult {
         old_only: Vec::with_capacity(CAPACITY),
@@ -68,17 +78,19 @@ pub fn verify(old_manifest: Manifest, new_manifest: Manifest) -> Option<DiffResu
     };
 
     // These both need to be peekable because we need to be able to view the next entry
-    // in the iterator, but we only want to consume it under specific conditions.
+    // in the iterators, but we only want to consume them under specific conditions.
     let mut old_iter = old_manifest.entries.into_iter().peekable();
     let mut new_iter = new_manifest.entries.into_iter().peekable();
 
     while let (Some(old), Some(new)) = (old_iter.peek(), new_iter.peek()) {
-        match old.path.cmp(&new.path) {
+        // We use str comparisons because that's how we handle sorting in the hasher.
+        match old.path.as_str().cmp(new.path.as_str()) {
             Ordering::Equal => {
-                // Even if they aren't different, both iterators must always be consumed here.
+                // Because the paths are equal both iterators are currently at the same
+                // position, and they must both be consumed to keep them in lockstep.
                 let old = old_iter.next().unwrap();
                 let new = new_iter.next().unwrap();
-                // We're in this branch because we've already found the paths to be equal.
+                // We're just checking for size or hash differences.
                 if old.size != new.size || old.hash.as_bytes() != new.hash.as_bytes() {
                     result.changed.push((old, new));
                 }
@@ -88,7 +100,7 @@ pub fn verify(old_manifest: Manifest, new_manifest: Manifest) -> Option<DiffResu
         }
     }
 
-    // At most only one of these will actually append any data.
+    // At most only one of these will append any data.
     result.old_only.extend(old_iter);
     result.new_only.extend(new_iter);
 
