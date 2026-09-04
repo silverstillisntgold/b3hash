c879036a8294e7aa29a9c6934ce80592de062b80
silverstillisntgold <nickdilla13@gmail.com>
2026-09-04 10:44:20 -0400

    sync latest

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # B3Hash
 
-**This crate is considered feature complete.**
-
 A crate for creating and validating directory hashfiles.
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 /*!
 # B3Hash
 
-**This crate is considered feature complete.**
-
 A crate for creating and validating directory hashfiles.
 */
 
